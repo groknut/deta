87e8a1227a9cbf034f5d2959f99cb843feaf7127
Monke <chikindin99@inbox.ru>
2026-03-06 17:53:11 +0800

    Added help flag and deleted trash

--- readers/csv_reader.go
+++ readers/csv_reader.go
@@ -1,19 +1,1 @@
 package readers
-
-//Модель для BubbleTea для csv файлов
-type readerCSV struct{
-	cursor int
-	rows chan []string
-	titles chan string	
-}
-
-
-//начало работы csv файла
-func StartCSV() {
-
-}
-
-//чтение csv файла в отдельной горутине и передача данных в модель для отображения
-func(r *readerCSV) ReadCSV() {
-
-}
--- util/start_cmd.go
+++ util/start_cmd.go
@@ -16,9 +16,15 @@ func StartUtil() error{
 	if quantityArgs < 2{
 		return errors.New("File not transferred")
 	}
+	if os.Args[1] == "-h" || os.Args[1] =="--help"{
+		fmt.Println(`Commands...`)
+		return nil
+	}
+
 
 	var typeFile string
 	splitFileName := strings.Split(os.Args[1],".")
+
 	lenghtNameFile := len(splitFileName) 
 	if lenghtNameFile == 2{
 		typeFile = splitFileName[1]
@@ -26,10 +32,14 @@ func StartUtil() error{
 		typeFile = splitFileName[lenghtNameFile-1]
 	}
 
-	
+	//Вызывать методы для обработки файлов вызывать здесь
 	switch typeFile{
 	case "csv":
+		fmt.Println(typeFile)
 	case "json":
+		fmt.Println(typeFile)
+	case "hex":
+		fmt.Println(typeFile)
 
 	default:
 		return errors.New("Undefine file extension")
