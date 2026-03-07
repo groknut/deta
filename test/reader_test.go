2075aa28b3dd1f6f6619b3090329196beca731d2
Monke <chikindin99@inbox.ru>
2026-03-07 22:43:58 +0800

    Finded bad and unworking code

--- test/reader_test.go
+++ test/reader_test.go
@@ -1,10 +1,23 @@
 package test
 
-import(
+import (
+	"context"
+	"deta/readers"
+	"path/filepath"
 	"testing"
-	"utils/readers"
 )
 
 func TestReaderCSV(t *testing.T) {
-	
+	path, _ := filepath.Abs(".")
+	path = filepath.Join(path,"file.csv")
+	readContext, readCancel := context.WithCancel(context.Background())
+
+	ctx := ro
+
+	title := make([]string,0)
+	rows :=  make([][]string,0)
+	model := readers.ModelReaderCSV{Path: path, 
+							Ctx: ctx,
+							Title: &title,
+							Rows: &rows}
 }
