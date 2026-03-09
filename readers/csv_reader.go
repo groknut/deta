0346acf8c5e76edeb8d5c88b7a70ca8c6a0830b3
Monke <chikindin99@inbox.ru>
2026-03-09 14:38:39 +0800

    Added table in reader

--- readers/csv_reader.go
+++ readers/csv_reader.go
@@ -9,6 +9,10 @@ import (
 	"strings"
 
 	"github.com/charmbracelet/lipgloss"
+	// "github.com/charmbracelet/bubbletable"
+
+	// "github.com/Evertras/bubble-table/table"
+	"github.com/evertras/bubble-table/table"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -22,10 +26,11 @@ type ModelReaderCSV struct {
 	Rows *[][]string
 	CtxCancel context.CancelFunc
 	Ctx context.Context
+	Table table.Model
 	Style struct{
-		title lipgloss.Style
-		item lipgloss.Style
-		cancel lipgloss.Style
+		Title lipgloss.Style
+		Item lipgloss.Style
+		Cancel lipgloss.Style
 	}
 }
 
@@ -38,8 +43,8 @@ func StartReaderCSV(ctx context.Context,ctxCancel context.CancelFunc, pathFile s
 							Title: &title,
 							Rows: &rows,
 							CtxCancel: ctxCancel}
-	model.Style.item = lipgloss.NewStyle().Background(lipgloss.Color("#C40361"))
-	model.Style.title = lipgloss.NewStyle().Background(lipgloss.Color("#8C0286"))
+	model.Style.Item = lipgloss.NewStyle().Background(lipgloss.Color("#C40361"))
+	model.Style.Title = lipgloss.NewStyle().Background(lipgloss.Color("#8C0286"))
 
 	model.Init()	
 	p := tea.NewProgram(model)
@@ -61,16 +66,7 @@ func StartReaderCSV(ctx context.Context,ctxCancel context.CancelFunc, pathFile s
 
 // Отрисовка модели 
 func (m ModelReaderCSV) View() string{
-	s := m.Style.title.Render(strings.Join(*m.Title, "\t"))+"\n"
-	for i, row := range *m.Rows{
-		cursor := " "
-		if m.Cursor == i{
-			cursor = ">"
-		}
-		s += cursor + strings.Join(row, "\t")+"\n"
-	}
-	s += "q - exit"
-	return s
+	return m.Table.View() + "\n\n↑/↓: navigate • q: quit"
 }
 
 // Обновление инфлормации в таблицы
@@ -81,20 +77,22 @@ func(m ModelReaderCSV) Update(msg tea.Msg) (tea.Model, tea.Cmd){
 		switch msg.String(){
 		case "ctrl+c", "q":
 			return m, tea.Quit
-		case "up":
-			if m.Cursor > 0{
-				m.Cursor--
-
-			}
-		case "down":
-			if m.Cursor < len(*m.Rows)-1{
-				m.Cursor++
-			}
+		case "up", "down":
+			
+				// newTable, cmd := m.Table.Update(msg)
+				// m.Table = newTable.(tea.Model)
+			var cmd tea.Cmd
+			m.Table, cmd = m.Table.Update(msg)
+			return m, cmd
 		}
+	case tea.WindowSizeMsg:
+		m.Table = m.Table.WithMaxTotalWidth(msg.Width)
+		m.Table = m.Table.WithPageSize(msg.Height - 5)
 	}
 
 	return m, nil
 }
+
 
 // Инициализируем нашу модель
 func(m ModelReaderCSV) Init() tea.Cmd{
@@ -102,7 +100,7 @@ func(m ModelReaderCSV) Init() tea.Cmd{
 }
 
 //Чтение csv файла
-func(m ModelReaderCSV) ReadCSV(){
+func(m *ModelReaderCSV) ReadCSV(){
 	file, err := os.Open(m.Path)
 	if err != nil{
 		m.CtxCancel()
@@ -135,6 +133,52 @@ func(m ModelReaderCSV) ReadCSV(){
 		m.CtxCancel()
 	}
 
+	width :=  len(*m.Title)
+	columns := make([]table.Column, width)
+	for i, title := range *m.Title{
+		maxWidth := width
+		for _, row := range *m.Rows{
+			if i < len(row) && len(row[i]) > maxWidth{
+				maxWidth = len(row[i])
+			}
+		}
+		// columns[i] = table.Column{
+		// 	Title: title,
+		// 	Width: maxWidth +2,
+		// }
+		columns[i] = table.NewColumn(title, title, maxWidth+2)
 
 
+	}
+
+	tableRows := make([]table.Row, len(*m.Rows))
+	for _, row := range *m.Rows {
+		rowData := table.RowData{}
+		for i := range columns{
+			if i < len(row){
+				rowData[(*m.Title)[i]] = row[i]
+			} else{
+				rowData[(*m.Title)[i]] = ""
+			}
+		}
+		// tableRows[i] = table.Row{
+		// 	Cells: cells,
+		// }
+		tableRows = append(tableRows, table.NewRow(rowData))
+	}
+	m.Table = table.New(columns).
+    WithRows(tableRows).
+    Focused(true).
+    WithPageSize(20)
+
+	m.Table = m.Table.HeaderStyle(m.Style.Title)
+	m.Table = m.Table.HighlightStyle(m.Style.Item)
+// m.Table = table.New(columns).
+//     WithRows(tableRows).
+//     WithStyles(table.Styles{
+//         Header:   m.Style.Title,
+//         Selected: m.Style.Item, // или Highlighted: m.Style.Item (зависит от версии)
+//     }).
+//     Focused(true).
+//     WithPageSize(20)
 }
