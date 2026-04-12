d4f91a2db52e5d9a091eadc7d69840f08602a29c
Monke <chikindin99@inbox.ru>
2026-04-12 06:34:04 +0000

    Updated count test

--- test/parse_test.go
+++ test/parse_test.go
@@ -8,4 +8,6 @@ import(
 
 func TestParse(t *testing.T){
 	fmt.Println(parse.Parse("CREATE TABLE orders (id PRIMARY KEY, user_id INTEGER, total_amount DECIMAL(10,2) );"))
+	fmt.Println(parse.Parse("CREATE TABLE user (id PRIMARY KEY, user_id INTEGER, total_amount INTEGER);"))
+
 }
