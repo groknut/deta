f2e82d935446de9045f24787f1f2c8ee24f0d084
Monke <chikindin99@inbox.ru>
2026-04-12 06:21:53 +0000

    Added test parse

---
+++ test/parse_test.go
@@ -0,0 +1,11 @@
+package test
+
+import(
+	"testing"
+	"fmt"
+	parse "deta/internal/parseSQL"
+)
+
+func TestParse(t *testing.T){
+	fmt.Println(parse.Parse("CREATE TABLE orders (id PRIMARY KEY, user_id INTEGER, total_amount DECIMAL(10,2) );"))
+}
