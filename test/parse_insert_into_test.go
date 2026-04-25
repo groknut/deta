90bb305e10faac86bb6ddd4efa5089381c6f97cf
Monke <chikindin99@inbox.ru>
2026-04-25 04:48:59 +0000

    Changed string for test, added test for index element

--- test/parse_insert_into_test.go
+++ test/parse_insert_into_test.go
@@ -7,8 +7,8 @@ import(
 )
 
 func TestInsertInto(t *testing.T){
-	fmt.Println(parse.Parse("INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice@com');"))
-	fmt.Println(parse.Parse("INSERT INTO users (name,  email) VALUES ('Alice Johnson', 'alice@com');").Query[0])
-	fmt.Println(parse.Parse("INSERT INTO users (name,   email) VALUES ('Alice Johnson', 'alice@com');").Query[1])
-	fmt.Println(parse.Parse("INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice@com');").InValue)
+	fmt.Println(parse.Parse("INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice@com'), ('Bob Mil', 'Bob@Com');"))
+	fmt.Println(parse.Parse("INSERT INTO users (name,  email) VALUES ('Alice Johnson', 'alice@com'), ('Bob Mil', 'Bob@Com');;").Query[0])
+	fmt.Println(parse.Parse("INSERT INTO users (name,   email) VALUES ('Alice Johnson', 'alice@com'), ('Bob Mil', 'Bob@Com');;").Query[1])
+	fmt.Println(parse.Parse("INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice@com'), ('Bob Mil', 'Bob@Com');;").InValue[1])
 }
