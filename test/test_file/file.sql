cc923599fe649b37027f23de8062afe588ef4226
Monke <chikindin99@inbox.ru>
2026-04-25 12:08:51 +0000

    Added test for rows for table

--- test/test_file/file.sql
+++ test/test_file/file.sql
@@ -1,9 +1,12 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email TEXT UNIQUE
+    name VARCHAR(100),
+    email TEXT UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO users (name, email) 
 VALUES 
-    ('Alice Johnson', 'alice@com');
+    ('Alice Johnson', 'alice@com'),
+    ('Bob Mil', 'Bob@com'),
+    ('Max', 'Max@com');
