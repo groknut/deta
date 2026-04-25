633b2e85cdd51418c3e41b9b7d6ba5fd91163395
Monke <chikindin99@inbox.ru>
2026-04-25 04:21:39 +0000

    Added new test to look elements in slices

--- test/parse_insert_into_test.go
+++ test/parse_insert_into_test.go
@@ -8,4 +8,7 @@ import(
 
 func TestInsertInto(t *testing.T){
 	fmt.Println(parse.Parse("INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice@com');"))
+	fmt.Println(parse.Parse("INSERT INTO users (name,  email) VALUES ('Alice Johnson', 'alice@com');").Query[0])
+	fmt.Println(parse.Parse("INSERT INTO users (name,   email) VALUES ('Alice Johnson', 'alice@com');").Query[1])
+	fmt.Println(parse.Parse("INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice@com');").InValue)
 }
