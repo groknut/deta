16de7dfa4195686eb77f08f53008a103985bc6cc
groknut <184639205+groknut@users.noreply.github.com>
2026-05-24 17:45:48 +0800

    feat: add binary reader model skeleton and file opener

---
+++ internal/readers/binary_reader.go
@@ -0,0 +1,91 @@
+package readers
+
+import (
+    "os"
+    "io"
+
+    tea "github.com/charmbracelet/bubbletea"
+    "github.com/evertras/bubble-table/table"
+
+    "deta/utils"
+)
+
+type hexVisibleMsg struct {
+    rows []table.Row
+}
+type binaryErrorMsg error
+
+type ModelReaderBinary struct {
+    file        *os.File
+    fileSize    int64
+    rowsPerPage int
+    cursorRow   int64
+    selectedRow int
+    offset      int64
+    table       table.Model
+    styles      utils.ReaderStyles
+}
+
+func (m *ModelReaderBinary) totalRows() int64 {
+    return 0
+}
+
+func (m *ModelReaderBinary) updateViewport() {}
+
+func (m *ModelReaderBinary) loadVisibleRows() tea.Cmd {
+    return nil
+}
+
+func (m *ModelReaderBinary) Init() tea.Cmd {
+    return nil
+}
+
+func (m *ModelReaderBinary) View() string {
+    return ""
+}
+
+func (m *ModelReaderBinary) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    return m, nil
+}
+
+type BinaryReader struct {
+    path  string
+    model *ModelReaderBinary
+}
+
+func NewBinaryReader() *BinaryReader {
+    return &BinaryReader{
+        model: &ModelReaderBinary{},
+    }
+}
+
+func (r *BinaryReader) Init(path string) error {
+    r.path = path
+    file, err := os.Open(path)
+    if err != nil {
+        return err
+    }
+    stat, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return err
+    }
+
+    r.model.file = file
+    r.model.fileSize = stat.Size()
+    r.model.rowsPerPage = 16
+    r.model.cursorRow = 0
+    r.model.styles = utils.DefaultStyles()
+    return nil
+}
+
+func (r *BinaryReader) Run() error {
+    p := tea.NewProgram(r.model)
+    if _, err := p.Run(); err != nil {
+        return err
+    }
+    if r.model.file != nil {
+        r.model.file.Close()
+    }
+    return nil
+}
