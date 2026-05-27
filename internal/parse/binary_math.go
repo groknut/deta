53c56c14a2e387d0a36496be681653694385c707
groknut <184639205+groknut@users.noreply.github.com>
2026-05-27 18:23:52 +0800

    Added function for calculate viewport

--- internal/parse/binary_math.go
+++ internal/parse/binary_math.go
@@ -7,3 +7,21 @@ func TotalRows(fileSize int64) int64 {
     }
     return (fileSize + BytesPerRow - 1) / BytesPerRow
 }
+
+// CalcViewport вычисляет смещение offset и индекс выделенной строки selectedRow
+// так, чтобы окно из rowsPerPage строк было отцентрировано вокруг cursorRow.
+func CalcViewport(cursorRow, totalRows, rowsPerPage int64) (offset int64, selectedRow int) {
+    half := rowsPerPage / 2
+    desiredStart := cursorRow - half
+    if desiredStart < 0 {
+        desiredStart = 0
+    } else if desiredStart+rowsPerPage > totalRows {
+        desiredStart = totalRows - rowsPerPage
+        if desiredStart < 0 {
+            desiredStart = 0
+        }
+    }
+    offset = desiredStart * BytesPerRow
+    selectedRow = int(cursorRow - desiredStart)
+    return
+}
