22770be7e8c29e63fb6a8bf318dd5a3d6b6eb376
Monke <chikindin99@inbox.ru>
2026-05-28 07:40:06 +0000

    Added print "Pass" in parse_test

--- test/parse_test.go
+++ test/parse_test.go
@@ -8,7 +8,10 @@ import(
 
 func TestParse(t *testing.T){
 	fmt.Println(parse.Parse("CREATE TABLE orders (id PRIMARY KEY, user_id INTEGER, total_amount DECIMAL(10,2) );").Query[1])
+	fmt.Println("Passed")
 	fmt.Println(parse.Parse("CREATE TABLE user (id PRIMARY KEY, user_id INTEGER, total_amount INTEGER);"))
+	fmt.Println("Passed")
 	fmt.Println(parse.Parse("CREATE TABLE orders (id PRIMARY KEY, user_id INTEGER, total_amount DECIMAL(10,2));"))
+	fmt.Println("Passed")
 
 }
