42272022b292ccbeedeb610e8baf6b5839056a64
groknut <184639205+groknut@users.noreply.github.com>
2026-05-28 10:10:15 +0800

    add json definite in getReader

--- internal/readers/factory.go
+++ internal/readers/factory.go
@@ -6,6 +6,7 @@ import "deta/pkg/interfaces"
 var FactoryTypeFile = map[string] func() interfaces.Reader{
 	"csv": func() interfaces.Reader { return NewCSVReader() },
 	"sql": func() interfaces.Reader { return NewSQLReader() },
+	"json": func() interfaces.Reader { return NewJSONReader() },
 }
 
 // Подает метод для вызова чтения файлов
