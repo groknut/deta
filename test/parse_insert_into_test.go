bfc791a87a77f9a77a93feff0d04a3b04a7c9e1c
Monke <chikindin99@inbox.ru>
2026-05-28 07:20:32 +0000

    Added print pass for sql

--- test/parse_insert_into_test.go
+++ test/parse_insert_into_test.go
@@ -8,6 +8,10 @@ import(
 
 func TestInsertInto(t *testing.T){
 	fmt.Println(parse.Parse("INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice@com'), ('Bob Mil', 'Bob@Com');"))
+	fmt.Println("Passed")
 	fmt.Println(parse.Parse("INSERT INTO users (name,  email) VALUES ('Alice Johnson', 'alice@com'), ('Bob Mil', 'Bob@Com');;").Query[0])
+	fmt.Println("Passed")
 	fmt.Println(parse.Parse("INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice@com'), ('Bob Mil', 'Bob@Com');;").InValue[1])
+	fmt.Println("Passed")
+
 }
